ce59c21a33b55ed32ee1250bafc653d69a9c8d16
ignaciodoo <icatalan@vedata.cl>
2021-09-13 19:33:32 +0000

    [ADD] Basic Module

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -2,18 +2,26 @@
 
 {
     'name': 'Odoo Academy',
+    
     'Summary': """App de Academia para entrenamiento en Odoo""",
+    
     'description': """
     Modulo de academia para gestionar entrenamiento:
     - cursos
     - sesiones
     - asistentes""",
-    'author': 'Ignacio',
-    'website': 'http://www.Odoo.com/',
-    'category': 'Training',
-    'version': '0.1',
-    'depends': ['base'],
-    'data': [],
-    'demo': [],
     
+    'author': 'Ignacio',
+    
+    'website': 'http://www.Odoo.com/',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [],
+    
+    'demo': [],
 }
