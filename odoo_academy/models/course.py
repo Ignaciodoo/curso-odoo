eb3afb9e9f842c650eac638ce69e616a5fcc90e3
ignaciodoo <icatalan@vedata.cl>
2021-09-17 19:39:02 +0000

    added decorators

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     _name = 'academy.course'
@@ -16,3 +17,22 @@ class Course(models.Model):
                                        ('avanzado', 'Avanzado')],
                             copy=False)
     active = fields.Boolean(string='Active', default=True)
+    
+    base_price = fields.Float(string='Base Price', default=0.00)
+    
+    additional_fee = fields.Float(string='Additional Fee', default=10.00)
+    
+    total_price = fields.Float(string='Total Rpice', readonly=True)
+    
+    @api.onchange('base_price', 'additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0:
+            raise UserError('El precio base no puede ser negativo.')
+        self.total_price = self.base_price + self.additional_fee
+    
+    @api.constrains('additional_fee')
+    def check_add_fee(self):
+        for record in self:
+            if record.additional_fee < 10:
+                raise ValidationError('El costo adicional no puede ser menos de 10. Costo adicional actual: %s' %record.additional_fee)
+        
