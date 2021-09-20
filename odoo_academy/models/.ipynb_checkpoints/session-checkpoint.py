687a7dbc728301dd8911848e81224289d67237ac
ignaciodoo <icatalan@vedata.cl>
2021-09-20 20:13:25 +0000

    fix session model 2

--- odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
@@ -2,14 +2,15 @@
 
 from odoo import models, fields, api
 
+
 class Session(models.Model):
     _name = 'academy.session'
     _description = 'Session info'
     
     course_id = fields.Many2one(comodel_name='academy.course',
-                              string='Course',
-                              ondelete='cascade',
-                              required=True)
+                                string='Course',
+                                ondelete='cascade',
+                                required=True)
     
     name = fields.Char(string='Title', related='course_id.name')
     
--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -8,9 +8,9 @@ class Session(models.Model):
     _description = 'Session info'
     
     course_id = fields.Many2one(comodel_name='academy.course',
-                              string='Course',
-                              ondelete='cascade',
-                              required=True)
+                                string='Course',
+                                ondelete='cascade',
+                                required=True)
     
     name = fields.Char(string='Title', related='course_id.name')
     
